8408143fd32b3d87a8774b7a5f3f0f2be21ca575
SharonHu1990 <sharonhu0813@yeah.net>
2015-09-18 17:29:26 +0800

    First initialization.

---
+++ FMDB-Demo/FMDB-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDB-Demo
+//
+//  Created by 胡晓阳 on 15/9/18.
+//  Copyright © 2015年 HXY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMDB-Demo/FMDB-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDB-Demo
+//
+//  Created by 胡晓阳 on 15/9/18.
+//  Copyright © 2015年 HXY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
